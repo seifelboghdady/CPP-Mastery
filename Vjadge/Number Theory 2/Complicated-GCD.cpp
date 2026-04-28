3cbf31296d59720b53cd56c1d9043c80b91d0c4a
seifelboghdady <seifelboghdady@std.mans.edu.eg>
2026-04-29 00:23:40 +0300

    #69547046 | seifelboghdady's solution for [CodeForces-664A] [Problem G]

---
+++ Vjadge/Number Theory 2/Complicated-GCD.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string a,b; cin>>a>>b;
+    if(a==b) cout<<a;
+    else cout<<1;
+
+}
