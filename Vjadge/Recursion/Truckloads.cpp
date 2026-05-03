2c73ee58e9ed5606f8194018543253d31f3c029c
seifelboghdady <seifelboghdady@std.mans.edu.eg>
2026-05-03 18:02:35 +0300

    Solve New: ...

---
+++ Vjadge/Recursion/Truckloads.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int divide(int num, int load){
+    if(num==load) return;
+    return num = num/2;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int numCrat, loadSize; 
+    while (cin>>numCrat>>loadSize)
+    {
+        // cout<<numCrat/loadSize+numCrat%loadSize<<endl;
+    }
+    
+
+}
