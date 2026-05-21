3d4f665ddd9e16f4ebb99604e4033e75a6c4b8d0
seifelboghdady <seifelboghdady@std.mans.edu.eg>
2026-05-21 17:55:35 +0300

    #70021816 | seifelboghdady's solution for [SPOJ-PT07Y] [Problem B]

---
+++ Vjadge/Graph/isTree.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj;
+vector<bool> visited;
+
+void DFS(int n){
+    visited[n]=1;
+    for (auto v : adj[n])
+    {
+        if(!visited[v]){
+            DFS(v);
+        }
+    }
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m; cin>>n>>m;
+    adj.assign(n+1,{});
+    visited.assign(n+1, 0);
+
+    for (int i = 1; i <= m; i++)
+    {
+        int x, y; cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    int ans=0;
+    for (int i = 1; i <= n; i++)
+    {
+        if(!visited[i]){
+            ans++;
+            DFS(i);
+        }
+    }
+    // cout<<ans;
+    cout<<(ans == 1 && m == n-1 ? "YES" : "NO")<<endl;
+}
