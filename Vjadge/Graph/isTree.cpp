e7378ba067adc7e671b5869070ef5d68f428dcb2
seifelboghdady <seifelboghdady@std.mans.edu.eg>
2026-05-23 18:19:40 +0300

    add test2

--- Vjadge/Graph/isTree.cpp
+++ Vjadge/Graph/isTree.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 vector<vector<int>> adj;
 vector<bool> visited;
+bool cycle = 0;
 
 void DFS(int n){
     visited[n]=1;
@@ -10,6 +11,23 @@ void DFS(int n){
     {
         if(!visited[v]){
             DFS(v);
+        }
+    }
+    
+}
+
+void DFS(int n, int p){
+    visited[n]=1;
+    for (auto v : adj[n])
+    {
+        if(v==p){
+            continue;
+        }
+
+        if(!visited[v]){
+            DFS(v, n);
+        }else{
+            cycle = 1;
         }
     }
     
@@ -29,14 +47,18 @@ int main() {
         adj[x].push_back(y);
         adj[y].push_back(x);
     }
-    int ans=0;
-    for (int i = 1; i <= n; i++)
-    {
-        if(!visited[i]){
-            ans++;
-            DFS(i);
-        }
-    }
+    // int ans=0;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     if(!visited[i]){
+    //         ans++;
+    //         DFS(i);
+    //     }
+    // }
     // cout<<ans;
-    cout<<(ans == 1 && m == n-1 ? "YES" : "NO")<<endl;
+    // cout<<(ans == 1 && m == n-1 ? "YES" : "NO")<<endl;
+    // cout<<( m == n-ans ? "YES" : "NO")<<endl;
+
+    DFS(1,0);
+    cout<<(!cycle ? "YES" : "NO")<<endl;
 }
