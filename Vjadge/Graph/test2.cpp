69e510b7bd7124ad05bb3fd9bc98ce0413900af7
seifelboghdady <seifelboghdady@std.mans.edu.eg>
2026-05-24 16:29:25 +0300

    New: Update func(-DFS-) to deal with Undirected Graph

--- Vjadge/Graph/test2.cpp
+++ Vjadge/Graph/test2.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 vector<vector<int>> adj;
-vector<bool> visited;
-
+vector<int> visited;
+bool cycle =0;
+//0 => not visited, 1 => visited and not stack, 2=>visited in stack
 void DFS(int n){
-    visited[n]=1;
+    visited[n]=2;
     for (auto v : adj[n])
     {
         if(!visited[v]){
             DFS(v);
+        }else if(visited[v]==2){
+            cycle=1;
         }
     }
-    
+    visited[n]=1;
 }
 
 int main() {
@@ -27,16 +30,17 @@ int main() {
     {
         int x, y; cin>>x>>y;
         adj[x].push_back(y);
-        adj[y].push_back(x);
+        // adj[y].push_back(x); remove this because Graph is Directed
     }
-    int ans=0;
-    for (int i = 1; i <= n; i++)
-    {
-        if(!visited[i]){
-            ans++;
-            DFS(i);
-        }
-    }
+    // int ans=0;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     if(!visited[i]){
+    //         ans++;
+    //         DFS(i);
+    //     }
+    // }
     // cout<<ans;
-    cout<<(ans == 1 && m == n-1 ? "YES" : "NO")<<endl;
+    DFS(1);
+    cout<<(cycle ? "YES" : "NO")<<endl;
 }
