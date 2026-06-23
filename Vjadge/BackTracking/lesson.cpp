603eadef7f59a9b83271b0d2037c85b68d87cee2
seifelboghdady <seifelboghdady@std.mans.edu.eg>
2026-06-23 03:08:52 +0300

    add new problem

---
+++ Vjadge/BackTracking/lesson.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> a;
+int x,n;
+int ans = 0;
+void go(int i, int sum){
+    //base case
+    if(i==n){
+        if(sum==x){
+            ans++;
+            return;
+        }
+    }
+
+    go(i+1, sum);
+    go(i+1, sum+a[i]);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin>>x>>n;
+    a.resize(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
+    go(0,0);
+
+}
