42d0dba5e99ed84d297e95b3b9476d6eb552c2b9
seifelboghdady <seifelboghdady@std.mans.edu.eg>
2026-06-23 03:20:04 +0300

    #70557971 | seifelboghdady's solution for [SPOJ-PUCMM005] [Problem A]

--- Vjadge/BackTracking/lesson.cpp
+++ Vjadge/BackTracking/lesson.cpp
@@ -7,10 +7,8 @@ int ans = 0;
 void go(int i, int sum){
     //base case
     if(i==n){
-        if(sum==x){
-            ans++;
-            return;
-        }
+        if(sum==x)ans++;
+        return;
     }
 
     go(i+1, sum);
@@ -21,12 +19,14 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cin>>x>>n;
+    cin>>n>>x;
     a.resize(n);
     for (int i = 0; i < n; i++)
     {
         cin>>a[i];
     }
     go(0,0);
+
+    cout<<(ans>=1 ? "YES":"NO");
 
 }
