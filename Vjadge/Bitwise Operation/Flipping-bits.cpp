60b6c5f5db57b872e60b0dd091df04212405601c
seifelboghdady <seifelboghdady@std.mans.edu.eg>
2026-06-30 12:09:07 +0300

    #70662872 | seifelboghdady's solution for [Problem F]

---
+++ Vjadge/Bitwise Operation/Flipping-bits.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int q; cin>>q;
+    while (q--)
+    {
+        uint32_t n; cin>>n;
+        cout<<~(n)<<endl;
+    }
+}
