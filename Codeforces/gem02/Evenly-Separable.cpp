fe6c2e56129a4e3cf34cddf593f1048bba27f9bd
seifelboghdady <seifelboghdady@std.mans.edu.eg>
2026-08-13 10:39:34 +0300

    new problem ....

--- Codeforces/gem02/Evenly-Separable.cpp
+++ Codeforces/gem02/Evenly-Separable.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-int main() {
+signed main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
@@ -10,7 +11,31 @@ int main() {
     {
         int n; cin>>n;
         vector<int>b(n);
+        int sum =0;
+        for (int i = 0; i < n; i++)
+        {
+            cin>>b[i];
+            sum+=b[i];
+        }
         
+        int cur=0;
+        int x= LLONG_MAX;
+        for (int i = 0; i < n-1; i++)
+        {
+            cur += b[i];
+            sum-=b[i];
+
+            if(sum == cur){
+                x=0;
+                break;
+            }
+
+            if(i+1 == (n-i-1)) continue;
+
+            if((sum-cur)%(i+1-(n-i-1))==0 && (sum-cur)/(i+1-(n-i-1))>=0)
+                x= min(x, (sum-cur)/(i+1-(n-i-1)));
+        }
+        cout<<(x==LLONG_MAX? -1: x)<<'\n';
     }
     
 
